9f4a980345879c4e66ad71451ed6dad9dcb9fa88
Leonard von Merzljak <l.v.merzljak@mailbox.org>
2021-10-22 17:22:30 +0200

    Add benchmark runner for query 1

---
+++ benchmark/run_benchmark_q1.py
@@ -0,0 +1,44 @@
+import subprocess
+import os
+import itertools
+
+path_to_build_directory = "/home/merzljak/async/build2"
+path_to_source_directory = "/home/merzljak/async"
+path_to_tpch_directory = "/nvmeSpace/merzljak/sf20"
+path_to_data_directory = "/nvmeSpace/merzljak/data/sf20"
+path_to_cxx_compiler = "/usr/bin/g++-11"
+path_to_query1_out = "/home/merzljak/async/benchmark/query1_out.csv"
+
+page_size_powers = [12, 14, 16, 18, 20, 22]
+num_threads = [1, 5, 10, 15, 20]
+num_entries_per_ring = [2, 4, 8, 16, 32, 64]
+
+query1_out = open(path_to_query1_out, "w")
+print_header = "true"
+
+for page_size_power in page_size_powers:
+    subprocess.run(["cmake", "-S", path_to_source_directory, "-B", path_to_build_directory, "-DCMAKE_BUILD_TYPE=Release",
+                   "-DASYNCHRONOUS_IO_PAGE_SIZE_POWER={}".format(page_size_power), "-DCMAKE_CXX_COMPILER={}".format(path_to_cxx_compiler)], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.run(
+        ["cmake", "--build", path_to_build_directory, "--clean-first"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    lineitemq1 = os.path.join(path_to_data_directory, "lineitemQ1.dat")
+    subprocess.run([os.path.join(path_to_build_directory, "executables", "load_data"),
+                   "lineitemQ1", os.path.join(path_to_tpch_directory, "lineitem.tbl"), lineitemq1], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    for threads, entries_per_ring in itertools.product(num_threads, num_entries_per_ring):
+        subprocess.run([os.path.join(path_to_build_directory, "executables", "tpch_q1"),
+                       lineitemq1, str(threads), str(entries_per_ring), str(1000), "true", "false", "false", print_header], check=True, stdout=query1_out, stderr=subprocess.PIPE)
+        print_header = "false"
+
+for page_size_power in [12, 16]:
+    subprocess.run(["cmake", "-S", path_to_source_directory, "-B", path_to_build_directory, "-DCMAKE_BUILD_TYPE=Release",
+                   "-DASYNCHRONOUS_IO_PAGE_SIZE_POWER={}".format(page_size_power), "-DCMAKE_CXX_COMPILER={}".format(path_to_cxx_compiler)], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.run(
+        ["cmake", "--build", path_to_build_directory, "--clean-first"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    lineitemq1 = os.path.join(path_to_data_directory, "lineitemQ1.dat")
+    subprocess.run([os.path.join(path_to_build_directory, "executables", "load_data"),
+                   "lineitemQ1", os.path.join(path_to_tpch_directory, "lineitem.tbl"), lineitemq1], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    for threads, entries_per_ring in itertools.product([10, 15, 20], num_entries_per_ring):
+        subprocess.run([os.path.join(path_to_build_directory, "executables", "tpch_q1"),
+                       lineitemq1, str(threads), str(entries_per_ring), str(1000), "true", "true", "false", "false"], check=True, stdout=query1_out, stderr=subprocess.PIPE)
